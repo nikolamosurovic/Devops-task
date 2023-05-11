c32aba76a9507b0aa7165a82fca11e2cc747d144
nikolam <nikola.mosurovic@providus.rs>
2023-05-11 16:30:14 +0200

    change prometheus.yml

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 from fastapi import FastAPI
+from fastapi_lambda import FastAPILambdaHandler
 from fastapi_prometheus import metrics, PrometheusMiddleware
 
 app = FastAPI()
@@ -7,12 +8,11 @@ app.add_middleware(PrometheusMiddleware)
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello from Things Solver"}
+    return {"message": "Hello from Things Solver!"}
 
 @app.get("/metrics")
 def read_metrics():
     return metrics()
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+handler = FastAPILambdaHandler(app)
+lambda_handler = handler.lambda_handler()
