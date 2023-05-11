0ca2fb81645dce2df787263ec2c5404ab4cba793
nikolam <nikola.mosurovic@providus.rs>
2023-05-11 15:50:42 +0200

    change prometheus.yml

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import json
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
+Instrumentator().instrument(app).expose(app)
 
 @app.get("/")
 def read_root():
