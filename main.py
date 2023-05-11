3b2c7f33c4b1faceb72c57dd2b5625cc5953918b
nikolam <nikola.mosurovic@providus.rs>
2023-05-11 17:48:37 +0200

    change main.py

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import json
 from fastapi import FastAPI
 from fastapi_lambda import FastAPILambdaHandler
 from prometheus_fastapi_instrumentator import Instrumentator
+from prometheus_fastapi_instrumentator.middleware import PrometheusMiddleware
 
 app = FastAPI()
 
 instrumentator = Instrumentator()
 instrumentator.instrument(app).expose(app)
+
+app.add_middleware(PrometheusMiddleware)
 
 @app.get("/")
 def read_root():
