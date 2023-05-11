33abf6a695926b3be716916d5bc1b24fd77041e8
nikolam <nikola.mosurovic@providus.rs>
2023-05-11 17:05:21 +0200

    change main.py

--- main.py
+++ main.py
@@ -4,15 +4,13 @@ from fastapi_lambda import FastAPILambdaHandler
 from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
-app.add_middleware(PrometheusMiddleware)
+
+instrumentator = Instrumentator()
+instrumentator.instrument(app).expose(app)
 
 @app.get("/")
 def read_root():
     return {"message": "Hello from Things Solver!"}
-
-@app.get("/metrics")
-def read_metrics():
-    return metrics()
 
 handler = FastAPILambdaHandler(app)
 lambda_handler = handler.lambda_handler()
