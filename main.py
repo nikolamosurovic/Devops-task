40ae711832288134a1531c1c94b5fc31a8aed7f2
nikolam <nikola.mosurovic@providus.rs>
2023-05-11 17:51:52 +0200

    change main.py

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 import json
 from fastapi import FastAPI
-from fastapi_lambda import FastAPILambdaHandler
-from prometheus_fastapi_instrumentator import Instrumentator
-from prometheus_fastapi_instrumentator.middleware import PrometheusMiddleware
+from fastapi_prometheus import metrics, PrometheusMiddleware
 
 app = FastAPI()
-
-instrumentator = Instrumentator()
-instrumentator.instrument(app).expose(app)
 
 app.add_middleware(PrometheusMiddleware)
 
@@ -15,5 +10,10 @@ app.add_middleware(PrometheusMiddleware)
 def read_root():
     return {"message": "Hello from Things Solver!"}
 
-handler = FastAPILambdaHandler(app)
-lambda_handler = handler.lambda_handler()
+@app.get("/metrics")
+def read_metrics():
+    return metrics()
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=80)
