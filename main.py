afde2b9a2b8f457e08252e0b85224f9474771024
nikolam <nikola.mosurovic@providus.rs>
2023-05-11 20:46:08 +0200

    change main.py

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
 import json
-from typing import Union
-from fastapi import FastAPI
-from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
-app = FastAPI()
-
-counter = Counter("app_requests_total", "Total number of requests")
-
-@app.get("/")
-def read_root():
-    counter.inc()
-    return {"message": "Hello from Things Solver!"}
-
-@app.get("/metrics")
-def read_metrics():
-    headers = {"Content-Type": CONTENT_TYPE_LATEST}
-    return generate_latest(), headers
+def lambda_handler(event, context):
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Things Solver!')
+    }
