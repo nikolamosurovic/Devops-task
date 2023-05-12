0fe8afc70d4c3757bbd6af34eaa4509eeb1cd11d
nikolam <nikola.mosurovic@providus.rs>
2023-05-13 00:40:49 +0200

    change handler.py

--- handler.py
+++ handler.py
@@ -1,13 +1,10 @@
 import json
 from fastapi import FastAPI
-import psutil
-from prometheus_client import CollectorRegistry, Gauge, generate_latest
 
-memory_usage = Gauge('memory_usage', 'Total memory usage in bytes')
 app=FastAPI()
 
 @app.get("/")
-def lambda_handler():
+def lambda_handler(event, context):
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Things Solver!')
