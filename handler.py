f0e75250b2ec59a5956f9b079e96670b072a3575
nikolam <nikola.mosurovic@providus.rs>
2023-05-13 00:30:54 +0200

    change handler.py

--- handler.py
+++ handler.py
@@ -12,26 +12,3 @@ def lambda_handler():
         'statusCode': 200,
         'body': json.dumps('Hello from Things Solver!')
     }
-
-
-@app.get("/metrics")
-def generate_prometheus_metrics():
-    # Get total memory usage
-    memory_stats = psutil.virtual_memory()
-    total_memory_usage = memory_stats.total
-
-    # Set the metric value
-    memory_usage.set(total_memory_usage)
-
-    # Create a new registry
-    registry = CollectorRegistry()
-
-    # Register the metric with the registry
-    registry.register(memory_usage)
-
-    # Generate the Prometheus text format
-    metrics = generate_latest(registry)
-
-    # Return the metrics as the response
-    print (metrics.decode('utf-8'))
-    return metrics.decode('utf-8')
